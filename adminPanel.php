bad9f86cd82041eca0ea85bdff8b522ff031e15f
Jakub <jakub.rola@gmail.com>
2019-02-01 10:39:46 +0100

    Dodalem miejsce na panele bocze oraz zmienilem formatowanie

--- adminPanel.php
+++ adminPanel.php
@@ -43,146 +43,116 @@ $partsOfSpeech =  $partsOfSpeechQuery->fetchAll();
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-    <title>Panel admina</title>
-    <meta charset="utf-8">
-    <meta name="Stona Jakub Rola" content="Język hiszpański nauka" />
+  <title>Panel admina</title>
+  <meta charset="utf-8">
+  <meta name="Stona Jakub Rola" content="Język hiszpański nauka" />
 	<meta name="keywords" content="Jakub Rola, Hiszpański, nauka języka" />
 	
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	<script src="JS/searchScript.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	
 	<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
 </head>
 <body class="bg-dark">
 
-	
+	<div class="container">		
+		<nav>
+      <div class="row">
+        <div class = "col-sm-12 p-4 m-2">
+          <!-- Nav pills -->
+          <ul class="nav nav-pills" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" data-toggle="pill" href="#addWord">Dodaj słowo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="pill" href="#verbs">Czasowniki</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="pill" href="#menu2">Menu 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+		</nav>
 
-	<div class="container">
-	
-		<!-- Nav pills -->
-		<ul class="nav nav-pills" role="tablist">
-			<li class="nav-item">
-			    <a class="nav-link active" data-toggle="pill" href="#addWord">Dodaj słowo</a>
-			</li>
-			<li class="nav-item">
-			    <a class="nav-link" data-toggle="pill" href="#verbs">Czasowniki</a>
-			</li>
-			<li class="nav-item">
-			    <a class="nav-link" data-toggle="pill" href="#menu2">Menu 2</a>
-			</li>
-		</ul>
-		<main class="container">	
-			<!-- Tab panes -->
-			<div class="tab-content col-sm-6">
-				<div id="addWord" class="container tab-pane active mt-2"> <!-- Adding word tab -->
-				
-					
-					<div class="">
-					
-						<?php //Display message if last action has been succeeded
-							if(isset($_SESSION['successMsg'])){
-								echo '<div class="alert alert-success alert-dismissible"><strong>Success!</strong>' . $_SESSION['successMsg'] . '</div>';
-								unset($_SESSION['successMsg']);
-							}
-						?>
-						
-						<form action="addWord.php" method="post">
-				
-							<div class="form-group">
-								<label for="word">Słowo po hiszpańsku:</label>
-								<input type="text" class="form-control" id="word" name="word">
-							</div>
-							
-							<div class="form-group">
-								<label for="translation">Tłumaczenie:</label>
-								<input type="text" class="form-control" id="translation" name="translation">
-							</div>
-							
-							<div class="form-group">
-								<label for="category">Wybierz kategorie:</label>
-								<select class="form-control" id="category" name="category">
-									<?php
-										foreach($categorys as $category){
-											echo '<option>'  . $category['category'] .'</option>';
-											//print_r($category['category']);
-										}										
-									?>
-								</select>
-							</div>					
-							
-							<div class="form-group">
-								<label for="PartOfSpeech">Część mowy:</label>
-								<div class="" id="PartOfSpeech">
-									
-									<?php
-										$i=1;
-										foreach($partsOfSpeech as $POS){										
-											echo '<div class="form-check-inline">';
-											echo '<label class="form-check-label" for="radio'.$i.'">';
-											echo '<input type="radio" class="form-check-input" id="radio'.$i.'" name="PoS" value="'.$POS['PartOfSpeech'].'" >'.$POS['PartOfSpeech'].'';
-											echo '</label></div>';
-										}
-									?>
-									
-								</div>
-								
-							</div>
-							
-							
-							<button type="submit" class="btn btn-success btn-block">Dodaj</button>
-							
-							<div  class="w-100 bg-danger p-2" id="allert">
-								<p><?= isset($_SESSION['errMsg']) ? $_SESSION['errMsg'] . $_SESSION['errDetail']: "" ?></p>
-								<?php 
-									unset($_SESSION['errMsg']);										
-								?>
-							</div>
-						
-						</form>
-					</div>
-				
-				</div>
-				<div id="verbs" class="container tab-pane fade"> <!-- Displaying verb tab --><br> 
-					<h3>Czasowniki</h3>
-					
-					<input class="form-control" id="searchScript" type="text" placeholder="Szukaj..">
-					<table class="table table-dark table-bordered">
-						<thead>
-						<tr>
-							<th>Słowo</th>
-							<th>Tłumaczenie</th>
-							<th>Kategoria</th>
-						</tr>
-						</thead>
-						<tbody id="wordsTable">
-						<?php
-							require_once 'database.php';
-							$verbsQuery = $db->query("SELECT slowo, tlumaczenie, kategoria FROM slownik WHERE czescMowy = 'czasownik';");
-							$slowa =  $verbsQuery->fetchAll();
-							foreach ($slowa as $slowo){
-								echo "<tr><td>{$slowo['slowo']}</td><td>{$slowo['tlumaczenie']}</td><td>{$slowo['kategoria']}</td></tr>";
-							}				  
-						?>
-						</tbody>
-					</table>
-						
-					
-				</div>
-				<div id="menu2" class="container tab-pane fade"><br>
-					<h3>Menu 2</h3>
-					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-				</div>
-			</div>
-	  </main>		
-			
-	
-	</div>
-	
-	
-	
-
+    <div class="row">
+      <div class="col-sm-3">
+        <h4>Widaomości:</h4>			
+      </div>
+      <!-- Tab panes -->
+      <div class="tab-content col-sm-6">
+        <div id="addWord" class="container tab-pane active mt-2"> <!-- Adding word tab -->
+          <div>					
+            <?php //Display message if last action has been succeeded
+              if(isset($_SESSION['successMsg'])){
+                echo '<div class="alert alert-success alert-dismissible"><strong>Success!</strong>' . $_SESSION['successMsg'] . '</div>';
+                unset($_SESSION['successMsg']);
+              }
+            ?>
+          </div>	
+          <form action="addWord.php" method="post">
+            <div class="form-group">
+              <label for="word">Słowo po hiszpańsku:</label>
+              <input type="text" class="form-control" id="word" name="word">
+            </div>
+                
+            <div class="form-group">
+              <label for="translation">Tłumaczenie:</label>
+              <input type="text" class="form-control" id="translation" name="translation">
+            </div>
+                
+            <div class="form-group">
+              <label for="category">Wybierz kategorie:</label>
+              <select class="form-control" id="category" name="category">
+                <?php
+                  foreach($categorys as $category){
+                  echo '<option>'  . $category['category'] .'</option>';
+                  //print_r($category['category']);
+                  }										
+                ?>
+              </select>
+            </div>							
+            <div class="form-group">
+              <label for="PartOfSpeech">Część mowy:</label>
+                <div class="" id="PartOfSpeech">
+                  <?php
+                    $i=1;
+                    foreach($partsOfSpeech as $POS){										
+                      echo '<div class="form-check-inline">';
+                      echo '<label class="form-check-label" for="radio'.$i.'">';
+                      echo '<input type="radio" class="form-check-input" id="radio'.$i.'" name="PoS" value="'.$POS['PartOfSpeech'].'" >'.$POS['PartOfSpeech'].'';
+                      echo '</label></div>';
+                    }
+                  ?>		
+                </div>								
+            </div>							
+                
+            <button type="submit" class="btn btn-success btn-block">Dodaj</button>
+                
+            <div  class="w-100 bg-danger p-2" id="allert">
+              <p><?= isset($_SESSION['errMsg']) ? $_SESSION['errMsg'] . $_SESSION['errDetail']: "" ?></p>
+                <?php 	unset($_SESSION['errMsg']);	?>
+            </div>
+              
+          </form>
+        </div>
+            
+        <div id="verbs" class="container tab-pane fade"> <!-- Displaying verb tab --><br> 
+                
+        </div>
+          <div id="menu2" class="container tab-pane fade"><br>
+            <h3>Menu 2</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <h4>Twoja ostania aktywność aktywność</h4>
+        
+        </div>
+      </div>
+	</div>	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </body>
 </html>
